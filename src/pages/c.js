035a8d167c21b8e4a2040715653efdb88bcb7abf
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-24 18:58:45 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -5,6 +5,58 @@ import ReactiveButton from 'reactive-button';
 
 
 function Form() {
+  if (!success) {
+          return setState({
+            status: 'failed',
+            message: 'Unable to submit your request.',
+          });
+        }
+
+        return setState({ status: 'succeed', message });
+      })
+      .catch(() =>
+        setState({
+          status: 'failed',
+          message: 'Unable to submit your request.',
+        })
+      );
+  };
+
+  if (state.status === 'succeed' && state.message) {
+    return (
+      <Card className="uil-m-auto uil-ta-center apply-form">
+        <Heading1 className="apply-text">Thank you!</Heading1>
+        <br />
+
+        {state.message.startsWith('Your DocSearch') ? (
+          <Text
+            className="uil-pv-8 uil-d-block apply-text"
+            aria-label="Request has already been processed"
+          >
+            {state.message}
+          </Text>
+        ) : (
+          <>
+            <Text
+              className="uil-pv-8 uil-d-block apply-text"
+              aria-label="Request will be processed"
+            >
+              {state.message} We'll get back to you at <strong>{email}</strong>{' '}
+              with the snippet you'll need to integrate into{' '}
+              <InlineLink href={url}>{url}</InlineLink>.
+            </Text>
+
+            <Text aria-label="recommendations" className="apply-text">
+              Please be patient, in the meantime, you can implement{' '}
+              <InlineLink href={withBaseUrl('docs/tips')}>
+                our recommendations for building a great DocSearch experience.
+              </InlineLink>
+            </Text>
+          </>
+        )}
+      </Card>
+    );
+  } 
   return (
     <Layout title="聯絡">
       <main>
@@ -55,7 +107,7 @@ function Form() {
               </p>
               <p>
                       <a href="https://github.com/">
-                    <ReactiveButton type={'submit'} idleText="Submit" />
+                    <ReactiveButton disabled={state.status === 'loading'} type={'submit'} idleText="Submit" />
                      </a>
 
               </p>
